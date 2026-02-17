1b695234d6a19d74440f715db80693094daad5ae
Pizzaman9988 <supershooter@live.no>
2026-02-17 13:01:02 +0100

    - Added splits as attribute of Individual struct.

--- src/Individual.jl
+++ src/Individual.jl
@@ -2,18 +2,19 @@ using Random
 Random.seed!(123)
 
 mutable struct Individual
-    genotype::Vector{Int} 
+    genotype::Vector{Int}
+    splits::Vector{Int}
     fitness::Float64
     selection_prob::Float64
 end
 
 function Individual(num_patients::Int)
     genotype = shuffle(1:num_patients) 
-    return Individual(genotype, 0.0, 0.0)
+    return Individual(genotype, [], 0.0, 0.0)
 end
 
 # Constructor for a specific genotype  -> used when creating offspring from crossover/mutation
-Individual(genotype::Vector{Int}) = Individual(genotype, 0.0, 0.0)
+Individual(genotype::Vector{Int}) = Individual(genotype, [], 0.0, 0.0)
 
 function initialize_population(pop_size::Int, num_patients::Int)
     population = [Individual(num_patients) for _ in 1:pop_size]
